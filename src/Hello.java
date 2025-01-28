b0fe6d7243cdd5f4ee29e33edc5bf4504f738ebd
Bryan3458 <bryan.a.martinez01@utrgv.edu>
2025-01-28 21:23:08 +0000

    Second Commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,5 @@
-public class Hello{
-
-    // Lets create our work area
+public class Hello {
     public static void main(String[] args) {
-       // print out hello world
-       System.out.println(x: "Hello World!"); 
+    System.out.println("Hello World");
     }
 }
